4c8bfff5583775eb959be4da1347fc68634437f2
Tess McGovern <ren.mcgovern@gmail.com>
2024-07-18 16:40:29 -0400

    server.js pretty well set up

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,10 @@
 const app = require('express').Router();
 const path = require('path');
+const express = require('express');
 
 
 app.get('/notes', (req, res) => {
     res.sendfile(path.join(__dirname, '../public/notes.html'));
-})
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-// used the mini-project for example
-
 const express = require('express');
 const path = require('path');
 const api = require('./routes/index.js');
@@ -8,20 +6,18 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use('/api', api);
 
 app.use(express.static('public'));
 
-// GET Route for homepage
-app.use('/', routes);
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
 
-
-// Wildcard route to direct users to a 404 page
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html')) //change this later
+  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
 );
 
 app.listen(PORT, () =>
